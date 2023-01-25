4625807f6ed9b9bf1593d511aaf127a9bbf4715e
Lesneo <64084644+simrgt@users.noreply.github.com>
2023-01-25 23:13:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,31 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
+import mysql.connector
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config["DEBUG"] = True
-app.config['MYSQL_HOST'] = 'bqpjqiutmrlk6tkmm9ef-mysql.services.clever-cloud.com'
-app.config['MYSQL_USER'] = 'usfyvwadlczjc1jj'
-app.config['MYSQL_PASSWORD'] = 'Jq2Sn2xCIZnqlEOZL1E4'
-app.config['MYSQL_DB'] = 'bqpjqiutmrlk6tkmm9ef'
+# app.config['MYSQL_HOST'] = 'bqpjqiutmrlk6tkmm9ef-mysql.services.clever-cloud.com'
+# app.config['MYSQL_USER'] = 'usfyvwadlczjc1jj'
+# app.config['MYSQL_PASSWORD'] = 'Jq2Sn2xCIZnqlEOZL1E4'
+# app.config['MYSQL_DB'] = 'bqpjqiutmrlk6tkmm9ef'
 
-mysql = MySQL(app)
+# mysql = MySQL(app)
 
 
 @app.route("/")
 def index():
+    db = mysql.connector.connect(
+        host="bqpjqiutmrlk6tkmm9ef-mysql.services.clever-cloud.com",
+        user="usfyvwadlczjc1jj",
+        password="Jq2Sn2xCIZnqlEOZL1E4",
+        database="bqpjqiutmrlk6tkmm9ef"
+    )
+    c= db.cursor()
+    c.execute('''INSERT INTO Personne VALUES(6)''')
+    db.commit()
+
+    db.close()
     # # Creating a connection cursor
     # cursor = mysql.connection.cursor()
     #
