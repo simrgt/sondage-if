41e458e5ae4af17ddb551e7ed94d169746ed0143
Lesneo <64084644+simrgt@users.noreply.github.com>
2023-01-25 20:42:45 +0100

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config["DEBUG"] = True
 
 @app.route("/")
