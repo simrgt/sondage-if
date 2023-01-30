3abfda9cced92051afe5af1713ce9a9ac5d95756
Lesneo <64084644+simrgt@users.noreply.github.com>
2023-01-30 18:27:33 +0100

    Ajout error login

--- app.py
+++ app.py
@@ -1,45 +1,46 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config["DEBUG"] = True
-# app.config['MYSQL_HOST'] = 'bqpjqiutmrlk6tkmm9ef-mysql.services.clever-cloud.com'
-# app.config['MYSQL_USER'] = 'usfyvwadlczjc1jj'
-# app.config['MYSQL_PASSWORD'] = 'Jq2Sn2xCIZnqlEOZL1E4'
-# app.config['MYSQL_DB'] = 'bqpjqiutmrlk6tkmm9ef'
+configDB = {
+    'host':"bqpjqiutmrlk6tkmm9ef-mysql.services.clever-cloud.com",
+    'user':"usfyvwadlczjc1jj",
+    'password':"Jq2Sn2xCIZnqlEOZL1E4",
+    'database':"bqpjqiutmrlk6tkmm9ef"
+}
 
 # mysql = MySQL(app)
-
+error = "Cette personne à déjà rempli ce sondage"
 
 @app.route("/")
 def index():
-    db = mysql.connector.connect(
-        host="bqpjqiutmrlk6tkmm9ef-mysql.services.clever-cloud.com",
-        user="usfyvwadlczjc1jj",
-        password="Jq2Sn2xCIZnqlEOZL1E4",
-        database="bqpjqiutmrlk6tkmm9ef"
-    )
-    c= db.cursor()
-    c.execute('''INSERT INTO Personne VALUES(6)''')
-    db.commit()
-
-    db.close()
-    # # Creating a connection cursor
-    # cursor = mysql.connection.cursor()
-    #
-    # # Executing SQL Statements
-    # cursor.execute(''' INSERT INTO Personne VALUES(1,"simon","regenwetter",19,"Draveil","sim@gmail.com") ''')
-    #
-    # # Saving the Actions performed on the DB
-    # mysql.connection.commit()
-    #
-    # # Closing the cursor
-    # cursor.close()
     return render_template("index.html")
 
-@app.route('/', subdomain ='app')
-def practice():
-    return "salut"
+@app.route("/inscription/", methods=['GET', 'POST'])
+def inscription() :
+    if request.method == "POST":
+        nom = request.form.get("nom")
+        prenom = request.form.get("prenom")
+        mail = request.form.get("mail")
+        db = mysql.connector.connect(**configDB)
+        c= db.cursor()
+
+        c.execute(f"SELECT nom, prenom, mail FROM Personne WHERE nom='{nom}' AND prenom={prenom} AND mail={mail}")
+        myresult = c.fetchall()
+        db.close()
+        if myresult is None:
+            return redirect(url_for('sondage'))
+        else :
+            return render_template("index.html", error=error)
+
+@app.route("/CGU/")
+def CGU():
+    return render_template("CGU.html")
+
+@app.route("/sondage/")
+def sondage():
+    return render_template("sondage.html")
