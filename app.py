db64a2ab08971c178626f47e26c77b43e740c793
Lesneo <64084644+simrgt@users.noreply.github.com>
2023-01-31 20:46:21 +0100

    Select relié entre eux

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_wtf import FlaskForm
+from wtforms import SelectField
 import mysql.connector
 import requests
-import simplejson
 import json
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config["DEBUG"] = True
+app.config['SECRET_KEY']='key'
 configDB = {
     'host':"bqpjqiutmrlk6tkmm9ef-mysql.services.clever-cloud.com",
     'user':"usfyvwadlczjc1jj",
@@ -18,6 +20,20 @@ configDB = {
 
 # mysql = MySQL(app)
 error = "Cette personne à déjà rempli ce sondage"
+
+class Groupe(FlaskForm):
+    db = mysql.connector.connect(**configDB)
+    c = db.cursor()
+    c.execute(f"SELECT alim_grp_code ,alim_grp_nom_fr FROM GroupeAliment")
+    groupe = SelectField('groupe', choices=c.fetchall())
+    c.execute(f"SELECT alim_ssgrp_code, alim_ssgrp_nom_fr FROM SousGroupeAliment")
+    sous_groupe = SelectField('sousGroupe', choices=c.fetchall())
+    c.execute(f"SELECT alim_ssssgrp_code, alim_ssssgrp_nom_fr FROM SousSousGroupeAliment")
+    sous_sous_groupe = SelectField('sousSousGroupe', choices=c.fetchall())
+    db.close()
+
+
+
 
 @app.route("/")
 def index():
@@ -63,7 +79,40 @@ def sondage():
         c.execute(f"SELECT NiveauScolaire FROM StatutScolaire")
         niveaux = c.fetchall()
         db.close()
-        print(type(niveaux))
+        form=Groupe()
 
 
-    return render_template("sondage.html", villes=villes, niveaux=niveaux)
+    return render_template("sondage.html", villes=villes, niveaux=niveaux, form=form)
+
+
+@app.route('/api/sousGroupe/<groupe>')
+def sousGroupe(groupe):
+    db = mysql.connector.connect(**configDB)
+    c = db.cursor()
+    c.execute(f"SELECT alim_ssgrp_code ,alim_ssgrp_nom_fr FROM SousGroupeAliment WHERE idGroupeAliment ={groupe}")
+    sous_groupes = c.fetchall()
+
+    sous_groupesArray = []
+    for sous_groupe in sous_groupes:
+        ssGrp = {}
+        ssGrp['id'] = sous_groupe[0]
+        ssGrp['name'] = sous_groupe[1]
+        sous_groupesArray.append(ssGrp)
+
+    return jsonify({'sousGroupe':sous_groupesArray})
+
+@app.route('/api/sousSousGroupe/<sousGroupe>')
+def sousSousGroupe(sousGroupe):
+    db = mysql.connector.connect(**configDB)
+    c = db.cursor()
+    c.execute(f"SELECT alim_ssssgrp_code ,alim_ssssgrp_nom_fr FROM SousSousGroupeAliment WHERE alim_ssgrp_code ={sousGroupe}")
+    sous_sous_groupes = c.fetchall()
+
+    sous_sous_groupesArray = []
+    for sous_sous_groupe in sous_sous_groupes:
+        ssssGrp = {}
+        ssssGrp['id'] = sous_sous_groupe[0]
+        ssssGrp['name'] = sous_sous_groupe[1]
+        sous_sous_groupesArray.append(ssssGrp)
+
+    return jsonify({'sousGroupe':sous_sous_groupesArray})
