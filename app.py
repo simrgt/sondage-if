81c524437812b2b3378bcabda2ef8ec9870002f1
Lesneo <64084644+simrgt@users.noreply.github.com>
2023-03-23 22:54:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,30 +10,27 @@ from apps.api.views import api
 from database.database import doSQLAdmin
 
 
-def create_app():
-    app = Flask(__name__)
-    # setup with the configuration provided
-    app.config.from_object('config.DevelopmentConfig')
 
-    # register blueprint
-    app.register_blueprint(main)
-    app.register_blueprint(sondage, url_prefix="/sondage")
-    app.register_blueprint(analyse, url_prefix="/analyse")
-    app.register_blueprint(api, url_prefix="/api")
+app = Flask(__name__)
+# setup with the configuration provided
+app.config.from_object('config.DevelopmentConfig')
 
-    # setup login manager
-    login_manager = LoginManager()
-    login_manager.login_view = 'login'
-    login_manager.init_app(app)
+# register blueprint
+app.register_blueprint(main)
+app.register_blueprint(sondage, url_prefix="/sondage")
+app.register_blueprint(analyse, url_prefix="/analyse")
+app.register_blueprint(api, url_prefix="/api")
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        rv = doSQLAdmin("SELECT * FROM user WHERE id = ?", (user_id,))
-        return User(rv[0][0], rv[0][1], rv[0][2]) if rv else None
+# setup login manager
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
-    return app
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    rv = doSQLAdmin("SELECT * FROM user WHERE id = ?", (user_id,))
+    return User(rv[0][0], rv[0][1], rv[0][2]) if rv else None
 
 
-if __name__ == "__main__":
-    create_app().run()
+
